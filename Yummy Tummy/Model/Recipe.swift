c02076c6475aeb1fd49e755523939e3b8842610f
admin <admin@admins-mac-mini.local>
2018-01-06 22:19:52 +0200

    add init to  Recipe

--- Yummy Tummy/Model/Recipe.swift
+++ Yummy Tummy/Model/Recipe.swift
@@ -5,9 +5,16 @@ class Recipe{
     var autor:String
     var imageUrl:String?
     
-    init(recipeText:String,autor:String) {
+    init(recipeText:String,autor:String,imageUrl:String?) {
         self.recipeText = recipeText
         self.autor = autor
+        self.imageUrl = imageUrl
+    }
+    
+    init(fromJson:[String:Any]) {
+        self.recipeText = fromJson["recipeText"]
+        self.autor = fromJson["autor"]
+        self.imageUrl = fromJson["imageUrl"]
     }
     
 }
