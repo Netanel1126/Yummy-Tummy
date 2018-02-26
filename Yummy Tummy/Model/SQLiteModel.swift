b1a3be18941119190062744b37faedb2f6a26f91
admin <admin@admins-mac-mini.local>
2018-02-26 23:21:02 +0200

    add sql open

--- Yummy Tummy/Model/SQLiteModel.swift
+++ Yummy Tummy/Model/SQLiteModel.swift
@@ -1,1 +1,20 @@
 import Foundation
+
+class SqLiteModel{
+    var database:OpaquePointer? = nil
+    let dbName = "YT_database.db"
+    
+    init() {
+        initDb()
+    }
+    
+    func initDb(){
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+            let path = dir.appendingPathComponent(dbName)
+            if sqlite3_open(path.absoluteString, &database) != SQLITE_OK{
+                print("Faild To Open DB File: \(path.absoluteString)")
+                return
+            }
+        }
+    }
+}
