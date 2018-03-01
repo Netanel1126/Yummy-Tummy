a4e329262e7c7a68fec884f7b51e851da66625bf
admin <admin@admins-mac-mini.local>
2018-03-01 13:33:09 +0200

    add segue to my recipe view controller

--- Yummy Tummy/ViewControllers/MyRecipeViewController.swift
+++ Yummy Tummy/ViewControllers/MyRecipeViewController.swift
@@ -4,14 +4,23 @@ class MyRecipeViewController: UIViewController {
 
     var titelText:String = ""
     var recipeText:String = ""
-    @IBOutlet weak var recipeLabel: UILabel!
+    var image:UIImage = UIImage(named: "Logo1")!
+    @IBOutlet weak var recipeImg: UIImageView!
+    @IBOutlet weak var recipeTextView: UITextView!
+    @IBOutlet weak var spiner: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = titelText
-        recipeLabel.text = recipeText
+        recipeTextView.text = recipeText
+        recipeImg.image = image
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        spiner.stopAnimating()
+        spiner.isHidden = true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
